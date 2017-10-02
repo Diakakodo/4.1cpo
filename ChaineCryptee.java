d7874ec193c01e9e8656cf000509b4f9629aadd5
KETAM Djillali <ketam.djalal@gmail.com>
2017-10-02 22:48:26 +0200

    ajotu du constructeur de la class

--- ChaineCryptee.java
+++ ChaineCryptee.java
@@ -2,4 +2,10 @@ public class ChaineCryptee
 {
 	private int decalage;
 	private String enClair;
+	public ChaineCryptee(String enCLair,int decalage)
+    {
+        // initialise instance variables
+        this.enClair=enClair;
+        this.decalage=decalage;
+    }
 }
