667859ed17d8d9f1bd1e4f7ec7920185b4a62797
KETAM Djillali <ketam.djalal@gmail.com>
2017-10-03 13:26:55 +0200

    traitement chaine vide

--- ChaineCryptee.java
+++ ChaineCryptee.java
@@ -12,4 +12,21 @@ public class ChaineCryptee
     {
         return enClair;
     }
+     public static char decaleCaractere ( char c , int decalage ) {
+     return (c < 'A' || c > 'Z' ) ? c : (char) ((( c - 'A' + decalage)%26 ) + 'A');
+    }
+    public String Crypte()
+    {
+		if (this.enClair == null){
+			return "chaine vide"; 
+		}else{
+        String ch = "";
+        int i;
+        char c;
+        for(i=0;i<enClair.length();i++)
+           { c=decaleCaractere(enClair.charAt(i),this.decalage); 
+             ch+= c;}
+        return ch;
+		}
+	}
 }
