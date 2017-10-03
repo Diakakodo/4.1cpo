c1d8ce43b84fbf6b22439760180ca0f1c040653d
KETAM Djillali <ketam.djalal@gmail.com>
2017-10-03 14:01:05 +0200

    Q7 et Q8 modifcation du constructeur

--- ChaineCryptee.java
+++ ChaineCryptee.java
@@ -1,32 +1,68 @@
+/**
+ * Décrivez votre classe ChaineCryptee ici.
+ *
+ * @author (KETAM)
+ * @version (02-10-2017)
+ */
 public class ChaineCryptee
 {
 	private int decalage;
-	private String enClair;
-	public ChaineCryptee(String enClair,int decalage)
+	private String enCryptee;
+	 /**
+     * Constructeur d'objets de classe ChaineCryptee
+     */
+	private ChaineCryptee(String enClair, int decalage)
     {
-        // initialise instance variables
-        this.enClair=enClair;
-        this.decalage=decalage;
+        // initialisation des variables d'instance
+        String str ="";
+        char c;
+        int i;
+        //traitement de la chaine si null => la chaine chaineCryptee reste null 
+        if(enClair != null) {
+            for(i = 0; i < enClair.length(); i++) {
+                c=decaleCaractere(enClair.charAt(i),this.decalage); 
+                str+= c;
+            }
+        }
+        this.enCryptee = str;
+        this.decalage = decalage;
     }
-    public String Decrypte()
+    
+    
+    public String crypte()
     {
-        return enClair;
+        return enCryptee;
     }
      public static char decaleCaractere ( char c , int decalage ) {
      return (c < 'A' || c > 'Z' ) ? c : (char) ((( c - 'A' + decalage)%26 ) + 'A');
     }
-    public String Crypte()
+    public String decrypte()
     {
-		if (this.enClair == null){
-			return "chaine vide"; 
-		}else{
-        String ch = "";
+        StringBuilder str = new StringBuilder();
         int i;
-        char c;
-        for(i=0;i<enClair.length();i++)
-           { c=decaleCaractere(enClair.charAt(i),this.decalage); 
-             ch+= c;}
-        return ch;
-		}
-	}
+        
+        for(i = 0; i < enCryptee.length(); i++) {
+            str.append(decaleCaractere(enCryptee.toUpperCase().charAt(i), - decalage));
+        }
+        return str.toString();
+    }
+    
+        public static ChaineCryptee deCryptee(String cryptee, int decalage) { 
+        
+        StringBuilder str = new StringBuilder();
+        int i;
+        
+        if(cryptee != null) {
+            for(i = 0; i < cryptee.length(); i++) {
+                str.append(decaleCaractere(cryptee.toUpperCase().charAt(i), - decalage));
+            }
+        }
+        ChaineCryptee chaine = new ChaineCryptee(str.toString(), decalage);
+        return chaine;
+    }
+    public static ChaineCryptee deEnClair(String enclair, int decalage) {
+        ChaineCryptee chaine = new ChaineCryptee(enclair, decalage);
+        return chaine;
+    }
+    
 }
